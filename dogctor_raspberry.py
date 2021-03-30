640bd68efda077a6e15a5b1554ae1291cc860aab
sungchan254@naver.com <sungchan254@naver.com>
2021-03-30 14:57:17 +0900

    tldus

--- dogctor_raspberry.py
+++ dogctor_raspberry.py
@@ -1,7 +1,7 @@
 from bluepy.btle import *
 import threading    
 import bluepy.btle
-from data_process import *
+# from data_process import *
 
 def findDevice () : 
     target_name = "BT04"   # target device name
@@ -35,15 +35,15 @@ class MyDelegate(DefaultDelegate):                    #Constructor (run once on 
 
     def upload (self) :
         self.time = self.currentTime 
-        Parce = Parcing()
-        Cam = Camera()
-        print('color...')
-        img = Cam.capture()
-        color = Parce.restroom(img)
-        food = Parce.restaurant(self.weightList)
-        data_send = make_dict(name1 = 'color', val1 = color, name2='eat', val2=food)
-        response = Parce.send_json(data_send)
-        print('response :', response.status_code)
+        # Parce = Parcing()
+        # Cam = Camera()
+        # print('color...')
+        # img = Cam.capture()
+        # color = Parce.restroom(img)
+        # food = Parce.restaurant(self.weightList)
+        # data_send = make_dict(name1 = 'color', val1 = color, name2='eat', val2=food)
+        # response = Parce.send_json(data_send)
+        # print('response :', response.status_code)
          
 
 class ScanDelegate(DefaultDelegate):
@@ -59,11 +59,12 @@ class ScanDelegate(DefaultDelegate):
 
 def rcv_data (device,  delegate) :
     timelimit = 20
+    last = -1
     chList = device.getCharacteristics()
-    # print ("Handle   UUID                                Properties")
-    # print ("-------------------------------------------------------")                 
-    # for ch in chList:
-    #     print ("  0x"+ format(ch.getHandle(),'02X')  +"   "+str(ch.uuid) +" " + ch.propertiesToString())
+    print ("Handle   UUID                                Properties")
+    print ("-------------------------------------------------------")                 
+    for ch in chList:
+        print ("  0x"+ format(ch.getHandle(),'02X')  +"   "+str(ch.uuid) +" " + ch.propertiesToString())
     device.writeCharacteristic(36, struct.pack('<bb', 0x01, 0x00), withResponse=True)
     while (True):    
         if device.waitForNotifications(1.0) :
@@ -74,46 +75,46 @@ def rcv_data (device,  delegate) :
                 delegate.upload()
             else :
                 pass
-        
 
 def main():
     threads = []
     #bluepy.btle.Debugging = True
     print("main")
-    while (True) :
-        peripheral = findDevice()
-        if peripheral :
-            break
-        else :
-            print ("The device cannot be found")
-            # time.sleep(10)
-    delegate = MyDelegate(peripheral)
-    peripheral.setDelegate(delegate)
-    print("Find Device")
+    while (True):
+        while (True) :
+            peripheral = findDevice()
+            if peripheral :
+                break
+            else :
+                print ("The device cannot be found")
+                # time.sleep(10)
+        delegate = MyDelegate(peripheral)
+        peripheral.setDelegate(delegate)
+        print("Find Device")
 
-    t = threading.Thread(target = rcv_data, args =  (peripheral,delegate ))
-    threads.append(t)
+        t = threading.Thread(target = rcv_data, args =  (peripheral,delegate ))
+        threads.append(t)
 
-    for iter in range(len(threads)) :
-        threads[iter].start()
+        for iter in range(len(threads)) :
+            threads[iter].start()
 
-    for iter in range(len(threads)) :
-        threads[iter].join()
+        for iter in range(len(threads)) :
+            threads[iter].join()
 
-    # rcv_data(peripheral,delegate )
+        # rcv_data(peripheral,delegate )
 
-    # for iter in range(len(Peripherals)) :
-    #     Peripherals[iter].setDelegate(MyDelegate(Peripherals[iter]) )
-    #     # Get MotionService
-    #     MotionService=Peripherals[iter].getServiceByUUID(motion_service_uuid)
-    #     # Get The Motion-Characteristics
-    #     MotionC = MotionService.getCharacteristics(motion_char_uuid)[0]
-    #     # Get The handle tf the  Button-Characteristics
-    #     hMotionC = MotionC.getHandle()+1
-    #     # Turn notifications on by setting bit0 in the CCC more info on:
-    #     Peripherals[iter].writeCharacteristic(hMotionC, struct.pack('<bb', 0x01, 0x00), withResponse=True)
+        # for iter in range(len(Peripherals)) :
+        #     Peripherals[iter].setDelegate(MyDelegate(Peripherals[iter]) )
+        #     # Get MotionService
+        #     MotionService=Peripherals[iter].getServiceByUUID(motion_service_uuid)
+        #     # Get The Motion-Characteristics
+        #     MotionC = MotionService.getCharacteristics(motion_char_uuid)[0]
+        #     # Get The handle tf the  Button-Characteristics
+        #     hMotionC = MotionC.getHandle()+1
+        #     # Turn notifications on by setting bit0 in the CCC more info on:
+        #     Peripherals[iter].writeCharacteristic(hMotionC, struct.pack('<bb', 0x01, 0x00), withResponse=True)
 
-    # print (Peripherals[iter].addr + " : Notification is turned on for Raw_data")
+        # print (Peripherals[iter].addr + " : Notification is turned on for Raw_data")
   
 
 if __name__ == '__main__':
