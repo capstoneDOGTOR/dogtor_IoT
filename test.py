41241745e7912e9c6aa5d8d8d631e3a9aa6426e8
dosoao <66048344+dosoao@users.noreply.github.com>
2021-04-26 15:59:12 +0900

    test7

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ camera = Camera()
 parcing = Parcing(uid)
 
 img = camera.capture()
-parcing.restaurant(img)
+parcing.restroom(img)
 
 weight_list = [150,150,150,140,140,140,130,130,130,120,120,120,100,100,100,70,70,70,40,40,40]
-parcing.restroom(weight_list)
+parcing.restaurant(weight_list)
