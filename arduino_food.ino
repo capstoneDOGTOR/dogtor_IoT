ff2e69643b4b722a74fcbec80663912e378d0737
dosoao <66048344+dosoao@users.noreply.github.com>
2021-04-28 16:30:07 +0900

    4m28d

--- arduino_food.ino
+++ arduino_food.ino
@@ -5,24 +5,24 @@
 // PD_SCK - pin #A0
 // RX = 2
 // TX = 3
-HX711 scale(A1, A0);
+// button = 8
+HX711 scale1(A0, A1);
 SoftwareSerial mySerial(3, 2);
+int button = 8;
 int weight = 0;
-int adc = 0; 
+int start = 1;
 
 void setup() {
-  scale.set_scale(2280.f);    // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();               // reset the scale to 0
+  scale1.set_scale(-405.0);    // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale1.tare();                
+  Serial.begin(9600);
   mySerial.begin(9600);
+  pinMode(button, INPUT_PULLUP);
 }
-
 void loop() {
-  adc = scale.get_units(10);
-  if(adc <0) {
-    adc = -adc;
+  if (digitalRead(button) == LOW) {
+    weight = scale1.get_units(1);
+    Serial.println(weight);
+    mySerial.print(weight);
   }
-  weight = int(adc)/5*5;
-
-  mySerial.print(weight);
-  delay(500);  
 }
