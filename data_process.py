b268cf12ceeb4c1b061ebe05fa71abad8dbb153e
dosoao <66048344+dosoao@users.noreply.github.com>
2021-05-10 17:59:03 +0900

    weight6

--- data_process.py
+++ data_process.py
@@ -215,7 +215,7 @@ class Parcing():
         self.send_json(dict, 'intake')
 
     def send_json(self, data, where):
-        URL = 'http://13.209.18.94:3000/info/' + where
+        URL = 'http://13.209.18.94:3000/' + where
 
         res = requests.post(URL, json=data, headers = {'Authorization':self.uid})
         #print('POST    :', res.status_code)
