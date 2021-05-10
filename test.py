f8d72f8df48e3e268ed737201c4c35f8c55f580b
dosoao <66048344+dosoao@users.noreply.github.com>
2021-05-10 17:41:35 +0900

    weight2

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ parcing = Parcing(uid)
 # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 # cv2.imwrite('./1.jpg',img)
 # parcing.restroom(img)
-
+print('start')
 parcing.weight()
 
 # weight_list = [150,150,150,140,140,140,130,130,130,120,120,120,100,100,100,70,70,70,40,40,40]
