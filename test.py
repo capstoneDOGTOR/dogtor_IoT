6132b4ae715625f6d6b00665ecd1d9dd99cf746b
dosoao <66048344+dosoao@users.noreply.github.com>
2021-05-15 14:18:33 +0900

    test

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ print('2.1')
 dog_weight = weight.weight()
 
 # 2.2 카메라 촬영
-# print('2.2')
+print('2.2')
 img = camera.capture()
 # img = cv2.imread('img.JPG')
 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
