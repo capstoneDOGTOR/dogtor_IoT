2e894b004ce60bfc3f03beb04966cd543b715af7
dosoao <66048344+dosoao@users.noreply.github.com>
2021-05-15 14:28:45 +0900

    iot

--- iot.py
+++ iot.py
@@ -37,7 +37,7 @@ class Weight():
                     cnt = Counter(weight_list)
                     break
 
-            delay(1000)
+            time.sleep(1000)
 
         GPIO.cleanup()
         return cnt[0]
