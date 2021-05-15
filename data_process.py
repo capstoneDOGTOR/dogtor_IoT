3722b7c3278f19a8289f87258d31b499d1169d02
sungchan1 <sungchan254@naver.com>
2021-05-15 08:39:06 +0100

    from_ras

--- data_process.py
+++ data_process.py
@@ -1,152 +1,88 @@
-import numpy as np
-import cv2
-#import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-import time
-import statistics
-import requests
-from picamera import PiCamera
-from io import BytesIO
-from PIL import Image
-
-class Camera():
-    def __init__(self):
-        self.camera = PiCamera()
-        self.camera.resolution = (640, 480)
-
-    def capture(self):
-        self.camera.start_preview()
-        stream = BytesIO()
-        time.sleep(2)
-
-        self.camera.capture(stream, 'jpeg')
-        img = Image.open(stream)
-        img = np.array(img)
-
-        return img
+from send import *
+from process_img import *
 
 class Parcing():
     def __init__(self, uid):
         self.cluster = 5
-        self.uid = uid
+        self.send = send(uid)
+        
 
-    def hsv2rgb(self, hsv):
-        h = hsv[0] * 2
-        s = hsv[1] / 255
-        v = hsv[2] / 255
-        r_ = 0
-        g_ = 0
-        b_ = 0
+    def restroom(self, img, weight):
 
-        c = v * s
-        h_ = h / 60
-        x = c * (1 - abs(h_ % 2 - 1))
-        if h_ >= 0 and h_ <= 1:
-            r_, g_, b_ = c, x, 0
-        elif h_ >= 1 and h_ <= 2:
-            r_, g_, b_ = x, c, 0
-        elif h_ >= 2 and h_ <= 3:
-            r_, g_, b_ = 0, c, x
-        elif h_ >= 3 and h_ <= 4:
-            r_, g_, b_ = 0, x, c
-        elif h_ >= 4 and h_ <= 5:
-            r_, g_, b_ = x, 0, c
-        elif h_ >= 5 and h_ <= 6:
-            r_, g_, b_ = c, 0, x
-        else:
-            r_, g_, b_ = 0, 0, 0
+        poo, pee = process_img(img, self.cluster)
 
-        m = v - c
-        r, g, b = r_ + m, g_ + m, b_ + m
+        #send to server
+        if poo['size'] != 0 or pee['size'] != 0:
+            dict = self.make_weight_dict(weight)
+            print(dict)
+            # self.send.send_json(dict, 'weight')
 
-        return np.array([r * 255, g * 255, b * 255])
-
-
-    def make_restaurant_dict(self, weight):
-        dict = {
-            'weight': str(weight),
-        }
-        return dict
-
-    def make_restroom_dict(self, rgb, size):
-        dict = {
-            'RGB': str(rgb),
-            'size': str(size)  # 소수점 세번째까지
-        }
-        return dict
-
-    def restroom(self, img):
-        # adjusting brightness
-        bright = 200 - np.mean(img)
-        img = np.clip(img + bright, 0, 255).astype(np.uint8)
-        # plt.imshow(img)
-        # plt.show()
-
-        # segmentation
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        flat_image = np.reshape(img, [-1, 3])
-        kmeans = KMeans(n_clusters=self.cluster)
-        kmeans.fit(flat_image)
-        labels = kmeans.labels_
-        labels = np.reshape(labels, img.shape[:2])
-        # plt.imshow(labels)
-        # plt.show()
-
-        # size, color
-        numlabels = np.arange(0, len(np.unique(labels)) + 1)
-        (hist, _) = np.histogram(labels, bins=numlabels)
-        hist = hist.astype('float')
-        sizes = hist / hist.sum()
-        hsvs = kmeans.cluster_centers_
-        # print('sizes    :', sizes)
-
-        # compare color
-        result_list = []
-        for i in range(len(hsvs)):
-            rgb = self.hsv2rgb(hsvs[i])
-            if rgb[0] < 180 or rgb[1] < 180 or rgb[2] < 180:
-                result_list.append((hsvs[i], sizes[i]))
-                # print('hsv      :', hsvs[i])
-                # print('rgb      :', rgb)
-                # plt.imshow(cv2.cvtColor(np.tile(hsvs[i], (100, 100, 1)).astype('uint8'), cv2.COLOR_HSV2RGB))
-                # plt.show()
-
-        small_rgb = None
-        big_rgb = None
-        white = np.array([255, 255, 255])
-        for i in range(len(result_list)):
-            color, size = result_list[i]
-            rgb = self.hsv2rgb(color).astype('uint8')
-
-            if color[2] < 150:
-                if big_rgb is None or np.linalg.norm(big_rgb - white) < np.linalg.norm(rgb - white):
-                    big_rgb = rgb
-                    rgb = '#' + str(hex(rgb[0]))[2:] + str(hex(rgb[1]))[2:] + str(hex(rgb[2]))[2:]
-                    dict = self.make_restroom_dict(rgb, round(size * 100, 3))
-                    print('poo  :', dict)
-                    self.send_json(dict, 'poo')
+        if poo['size'] != 0:
+            rgb = poo['rgb'].astype('uint8')
+            hsv = poo['hsv'].astype('uint8')
+            size = round(poo['size']* 100, 3)
+            color = hsv2color(hsv, rgb)
+            if color == 'brown':
+                flag = True
             else:
-                if small_rgb is None or np.linalg.norm(small_rgb - white) < np.linalg.norm(rgb - white):
-                    rgb = '#' + str(hex(rgb[0]))[2:] + str(hex(rgb[1]))[2:] + str(hex(rgb[2]))[2:]
-                    dict = self.make_restroom_dict(rgb, round(size * 100, 3))
-                    print('pee  :', dict)
-                    self.send_json(dict, 'pee')
+                flag = False
+
+            send_rgb = '#' + str(hex(rgb[0]))[2:] + str(hex(rgb[1]))[2:] + str(hex(rgb[2]))[2:]
+            send_hsv = str(hsv[0]) + '/' + str(hsv[1]) + '/'+ str(hsv[2])
+
+            dict = self.make_restroom_dict(rgb, hsv, size, color, flag)
+            print('poo  :', dict)
+            self.send.send_json(dict, 'poo')
+
+        if pee['size'] != 0:
+            rgb = pee['rgb'].astype('uint8')
+            hsv = pee['hsv'].astype('uint8')
+            size = round(pee['size']* 100, 3)
+            color = hsv2color(hsv)
+            if color == 'yellow':
+                flag = True
+            else:
+                flag = False
+
+            send_rgb = '#' + str(hex(rgb[0]))[2:] + str(hex(rgb[1]))[2:] + str(hex(rgb[2]))[2:]
+            send_hsv = str(hsv[0]) + '/' + str(hsv[1]) + '/' + str(hsv[2])
+
+            dict = self.make_restroom_dict(rgb, hsv, size, color, flag)
+            print('pee  :', dict)
+            self.send.send_json(dict, 'pee')
+
 
     def restaurant(self, weight_list):
         weights = np.array(weight_list)
+        max = weight_list[0]
+
 
         quantile = np.percentile(weights, [25, 75], interpolation='nearest')
         iqr = quantile[1] - quantile[0]
         outlier_max = iqr * 1.5 + quantile[1]
         result = weights[np.where(weights <= outlier_max)]
-        dict = self.make_restaurant_dict(result.max() - result.min())
+        dict = self.make_restaurant_dict(max- result.min())
         print(dict)
-        self.send_json(dict, 'intake')
+        self.send.send_json(dict, 'intake')
 
-    def send_json(self, data, where):
-        URL = 'http://13.209.18.94:3000/info/' + where
+    def make_weight_dict(self, weight):
+        dict = {
+            'weight': str(round(weight, 0)),
+        }
+        return dict
 
-        res = requests.post(URL, json=data, headers = {'Authorization':self.uid})
-        #print('POST    :', res.status_code)
-        #print(res.text)
-        return res
+    def make_restaurant_dict(self, weight):
+        dict = {
+            'amountOfMeal': str(weight)
+        }
+        return dict
+
+    def make_restroom_dict(self, rgb, hsv, size, color, flag):
+        dict = {
+            'RGB': str(rgb),
+            'HSV': str(hsv),
+            'size': str(size),  # 소수점 세번째까지
+            'color': color,
+            'flag': flag
+        }
+        return dict
--- dogctor_raspberry.py
+++ dogctor_raspberry.py
@@ -4,10 +4,11 @@ import bluepy.btle
 from data_process import *
 import bluetooth
 from bluetooth import *
+import subprocess
+import os
 
 def setWifi():
     uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-
     uid =""
     wifi_name = ""
     wifi_password = ""
@@ -17,7 +18,6 @@ def setWifi():
     server_sock=BluetoothSocket( RFCOMM )
     server_sock.bind(('',PORT_ANY))
     server_sock.listen(1)
-
     port = server_sock.getsockname()[1]
 
      # 블루투스 서비스를 Advertise
@@ -27,29 +27,54 @@ def setWifi():
     # 클라이언트가 연결될 때까지 대기
     client_sock, client_info = server_sock.accept()
     print('accepted')
-    while True:          
+    while (True):          
         print("Accepted connection from ", client_info)
         try:
             # 들어온 데이터를 역순으로 뒤집어 전달
             data = client_sock.recv(1024)
-            if len(data) == 0: break
-            print(type(data))
-            # data = data.decode()
+            if len(data) == 0: 
+                if uid != "":
+                    ps = subprocess.Popen(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                    try:
+                        output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
+                        print(output)
+                        if "ESSID:\""+wifi_name+"\"" in output  :
+                            return
+                        else:
+                            os.system('iwconfig ' + interface + ' essid ' + wifi_name + ' key ' + wifi_password)
+                    except subprocess.CalledProcessError:
+                        # grep did not match any lines
+                        print("No wireless networks connected")
+                break
+  
+            data = data.decode()
             if "from_app" in data:
-                print(type(data)
-               # data = data.decode()
                 array  =  data.split('/')
                 uid = array[1]
                 wifi_name = array[2]
                 wifi_password = array[3]
-                os.system('sed \'/}$/a\\network={\\n        ssid=\"'+wifi_name+'\"\\n        psk=\"'+wifi_password+'\"\\n        key_mgmt=WPA-PSK\\n        disabled=1\\n}\' /etc/wpa_supplicant/wpa_supplicant.conf')
-                os.system('reboot')
-            print("uid          :" +uid)
-            print("wifi_name    :"+wifi_name)
-            print("wifi_password:"+wifi_password)
+                # os.system('sed \'/}$/a\\network={\\n        ssid=\"'+wifi_name+'\"\\n        psk=\"'+wifi_password+'\"\\n        key_mgmt=WPA-PSK\\n        disabled=1\\n}\' /etc/wpa_supplicant/wpa_supplicant.conf')
+                # os.system('reboot')
+                print("uid          :" +uid)
+                print("wifi_name    :"+wifi_name)
+                print("wifi_password:"+wifi_password)
+                client_sock.send(data[::-1])
+                ps = subprocess.Popen(['iwconfig'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                try:
+                    output = subprocess.check_output(('grep', 'ESSID'), stdin=ps.stdout)
+                    print(output)
+                    if "ESSID:\""+wifi_name+"\"" in output  :
+                        return
+                    else:
+                        os.system('iwconfig ' + interface + ' essid ' + wifi_name + ' key ' + wifi_password)
+                except subprocess.CalledProcessError:
+                    # grep did not match any lines
+                    print("No wireless networks connected")
 
+               
 
-            client_sock.send(data[::-1])
+                
+       
         except IOError:
             print("disconnected")
             client_sock.close()
@@ -88,30 +113,39 @@ class MyDelegate(DefaultDelegate):
         self.time = time.time()
         self.currentTime = 0
         self.weightList = []
-        self.parce = Parcing("ddd")
+        self.parcing = Parcing(uid)
         self.cam = Camera()
+        
      #func is called on notifications
+
     def handleNotification(self, cHandle, data):     
         print("Get Data :" ,data.decode('utf-8'), "g")
         self.currentTime = time.time()
-        self.weightList.append(int(data.decode ('utf-8')))
-        if (self.currentTime - self.time > 20) :
-            self.upload()
+        if data.startswith("max"):
+            self.weight_list = []
+            self.weight_list.append(data.split('/')[1])
+        elif data.startswith("end") :
+            self.parcing.restaurant(weight_list)
+        else :
+            self.weight_list.append(data)
+           
 
-    def upload (self) :
-        print("Data Analysis...")
-        try :
-            self.time = self.currentTime 
-            img = self.cam.capture()
-            color = self.parce.restroom(img)
-            food = self.parce.restaurant(self.weightList)
-            self.weightList = []
-            print("Data Analysis Done")
-            data_send = make_dict(name1 = 'color', val1 = color, name2='eat', val2=food)
-            response = self.parce.send_json(data_send)
-            print('response :', response.text)
-        except :
-            pass
+
+    # def upload (self) :
+    #     print("Data Analysis...")
+    #     try :
+    #         self.time = self.currentTime 
+    #         img = self.cam.capture()
+    #         color = self.parce.restroom(img)
+    #         food = self.parce.restaurant(self.weightList)
+    #         self.weightList = []
+    #         print("Data Analysis Done")
+    #         data_send = make_dict(name1 = 'color', val1 = color, name2='eat', val2=food)
+    #         response = self.parce.send_json(data_send)
+    #         print('response :', response.text)
+    #     except :
+    #         pass
+
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -148,8 +182,30 @@ def rcv_data (device,  delegate) :
                 delegate.upload()
             else :
                 pass
+def capture (uid) :
+    parcing = Parcing(uid)
+    weight = Weight()
+    camera = Camera()
+    while(True) :
+        # 2.1 무게 측정
+        print('weight')
+        dog_weight = weight.weight()
+    
+        # 2.2 카메라 촬영
+        print('capture')
+        img = camera.capture()
+        # img = cv2.imread('img.JPG')
+        # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite('./sample1.jpg', img)
+
+        # 2.3
+        print('analysis')
+    global uid 
+
 
 def main():
+    global uid 
+    setWifi()
     threads = []
     #bluepy.btle.Debugging = True
     while (True):
@@ -165,15 +221,12 @@ def main():
         delegate = MyDelegate(peripheral)
         peripheral.setDelegate(delegate)
         print("Find Device")
-        t = threading.Thread(target = rcv_data, args =  (peripheral,delegate ))
-        threads.append(t)
-        t = threading.Thread(target = setWifi)
-        threads.append(t)
-        for iter in range(len(threads)) :
+      u  threads.append(threading.Thread(target = rcv_data, args =  (peripheral,delegate ))) #receive data from Arduino
+        threads.append(threading.Thread(target = capture, args = id))                        #capture 
+        for iter in range(len(threads)) :   
             threads[iter].start()
         for iter in range(len(threads)) :
             threads[iter].join()
-
 
 if __name__ == '__main__':
     main()
