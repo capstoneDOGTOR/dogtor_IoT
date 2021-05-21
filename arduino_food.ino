e96139354c8c245373981a04bc553846d26138af
dosoao <66048344+dosoao@users.noreply.github.com>
2021-05-21 22:09:13 +0900

    bugfix

--- arduino_food.ino
+++ arduino_food.ino
@@ -8,7 +8,7 @@
 // button = 2
 HX711 scale1(A0, A1);
 SoftwareSerial mySerial(8, 7);
-int button = 2;
+int button = 2; // interrupt pin
 
 flag = 0
 weight = 0
@@ -22,48 +22,52 @@ void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
   pinMode(button, INPUT_PULLUP);
-  attachInterrupt(0, buttoninterrupt, FALLING);
+  attachInterrupt(0, buttoninterrupt, FALLING); // interrupt num, funcion name, mode:Falling(HIGH->LOW)
 }
 
 void loop() {
 
-    if (flag == 1) {
+    if (flag == 1) { // wait
       before_weight = scale1.get_units(5);
       delay(3000);
-      now_weight = scale1.get_units(5);
-      if (before_weight != now_weight) {
-        start_time = (unsigned long)millis();
-        flag = 2;
+      now_weight = wscale1.get_units(5);
+
+      if (before_weight != now_weight) { // dog does touch his/her food
+        if (now_weight > 0) {
+          start_time = (unsigned long)millis();
+          flag = 2;
+        }
       }
-      else {
+      else {                            // dog does not touch his/her food
         delay(3000);
       }
     }
 
-    if (flag == 2) {
-        Serial.println("max/" + String(weight));
-        mySerial.print("max/" + String(weight));
+    if (flag == 2) { // start
+        Serial.println("max/" + String(max_weight));
+        mySerial.print("max/" + String(max_weight));
         flag = 3;
     }
 
-    if (flag == 3) {
+    if (flag == 3) { // send
         now_time = (unsigned long)millis();
         weight = scale1.get_units(5);
-        if (now_time - start_time < eating_time) {
+        if (now_time - start_time < eating_time) { // eating for eating_time
           Serial.println("data/" + String(weight));
           mySerial.print("data/" + String(weight));
         }
-        else {
+        else {                                     // stop eating
           max_weight = weight;
           Serial.println("end");
           mySerial.print("end");
           flag = 1;
         }
-        delay(1000);
+        delay(1000); // once for 10 seconds.
     }
 }
 
-void buttoninterrupt() {
-  max_weight = scale1.get_units(5);
-  flag = 1;
+void buttoninterrupt() { // interrupt, user push the button
+    // use debounce if it doesnt't work well
+    max_weight = scale1.get_units(5);
+    flag = 1;
 }
